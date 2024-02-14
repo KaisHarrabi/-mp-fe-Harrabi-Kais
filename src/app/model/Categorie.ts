95c1dac98952f9f8512e09f8f1bd5957a840dd4a
HarrabiKais <harrabikais87@gmail.com>
2024-02-14 13:20:05 +0100

    ng-gestion-produits

---
+++ src/app/model/Categorie.ts
@@ -0,0 +1,11 @@
+export class Categorie {
+  id: number | undefined;
+  code: string | undefined;
+  libelle: string | undefined;
+
+  constructor() {
+    this.id = 0;
+    this.code = '';
+    this.libelle = '';
+  }
+}
